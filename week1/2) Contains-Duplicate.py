d3e825cf1bb1470af666d5967171d35d7268a74a
Meet S <meetshinde.official@gmail.com>
2026-03-20 14:35:55 +0000

    dsa: Completed two problems

---
+++ week1/2) Contains-Duplicate.py
@@ -0,0 +1,12 @@
+#My approach
+class Solution:
+    def containsDuplicate(nums):
+        nums_t=set(nums)
+        if(len(nums_t)==len(nums)):
+            return True
+        else:
+            return False
+#A bit cleaner version
+class Solution:
+    def containsDuplicate(nums):
+        return len(set(nums)) != len(nums)
