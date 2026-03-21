a75d18fe7551fcee01e0b5aa6e915d47dd4b7bbe
Meet S <meetshinde.official@gmail.com>
2026-03-21 15:02:38 +0000

    dsa: Completed 4th probelm

---
+++ week1/4) Replace-with-greatest-element.py
@@ -0,0 +1,14 @@
+#My approach
+#Couldnt find an approach
+
+#Best way
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        right_max=-1
+        for i in range(len(arr)-1, -1, -1):
+            current=arr[i]
+            arr[i]=right_max
+            right_max=max(right_max, current)
+        return arr
+
+#Note: Try to visualize the transfers.
