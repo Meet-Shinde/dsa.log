a2ab7b80738415d88f9a7c3664c4e807331c96fd
Meet S <meetshinde.official@gmail.com>
2026-03-21 16:02:32 +0000

    dsa: Completed 5th problem

---
+++ week1/6) Majority element.py
@@ -0,0 +1,29 @@
+#My approach (actually got it right)
+class Solution:
+    def majorityElement(nums):
+        count = {}
+        for val in nums:
+            count[val] = count.get(val, 0) + 1
+        for res in count:
+            if count[res] > (len(nums) / 2):
+                return res
+#T: O(n)
+#S: O(n)
+
+#Best approach (Boyer-Moore Algorithm)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
+#T: O(n)
+#S: O(1)
+
+#Note: Again for frequencies, use hashmap/dict
